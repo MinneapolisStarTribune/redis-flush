118e322101ed564fee06c9b6fbc19c2e8585966d
Chris Harrington <chris.harrington@startribune.com>
2022-01-28 11:39:14 -0600

    OPS-526: Simplify flag handling and use one common context var

--- cmd.go
+++ cmd.go
@@ -8,39 +8,31 @@ import (
 	"github.com/go-redis/redis/v8"
 )
 
-var paramFlushAll bool
-var paramHost string
-var paramPort int
-var paramASync bool
-var paramPassword string
-var paramDB int
-
-func init() {
-	flag.BoolVar(&paramFlushAll, "all", false, "Use FLUSHALL instead of FLUSHDB")
-	flag.StringVar(&paramHost, "host", "localhost", "Hostname of primary Redis endpoint")
-	flag.IntVar(&paramPort, "port", 6379, "Port of Redis cluster")
-	flag.BoolVar(&paramASync, "async", false, "Don't wait for completion")
-	flag.StringVar(&paramPassword, "password", "", "Password for authentication, if enabled")
-	flag.IntVar(&paramPort, "db", 0, "Index of database to flush (use -all to flush all)")
-	flag.Parse()
-}
-
 func main() {
+	ctx := context.Background()
+	paramFlushAll := flag.Bool("all", false, "Use FLUSHALL instead of FLUSHDB")
+	paramHost := flag.String("host", "localhost", "Hostname of primary Redis endpoint")
+	paramPort := flag.Int("port", 6379, "Port of Redis cluster")
+	paramASync := flag.Bool("async", false, "Don't wait for completion")
+	paramPassword := flag.String("password", "", "Password for authentication, if enabled")
+	paramDB := flag.Int("db", 0, "Index of database to flush (use -all to flush all)")
+	flag.Parse()
+
 	var res *redis.StatusCmd
 	c := redis.NewClient(&redis.Options{
-		Addr:     fmt.Sprintf("%s:%d", paramHost, paramPort),
-		Password: paramPassword,
-		DB:       paramDB,
+		Addr:     fmt.Sprintf("%s:%d", *paramHost, *paramPort),
+		Password: *paramPassword,
+		DB:       *paramDB,
 	})
 	switch {
-	case paramFlushAll && paramASync:
-		res = c.FlushAllAsync(context.Background())
-	case !paramFlushAll && paramASync:
-		res = c.FlushDBAsync(context.Background())
-	case paramFlushAll && !paramASync:
-		res = c.FlushAll(context.Background())
-	case !paramFlushAll && !paramASync:
-		res = c.FlushDB(context.Background())
+	case *paramFlushAll && *paramASync:
+		res = c.FlushAllAsync(ctx)
+	case !*paramFlushAll && *paramASync:
+		res = c.FlushDBAsync(ctx)
+	case *paramFlushAll && !*paramASync:
+		res = c.FlushAll(ctx)
+	case !*paramFlushAll && !*paramASync:
+		res = c.FlushDB(ctx)
 	}
 	if res.Err() != nil {
 		panic(res.Err())
